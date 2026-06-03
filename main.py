182bca6bc9934b9f6f13f9846b714ba2fd541ce2
Sirusdog <sirusdog611@gmail.com>
2026-06-03 16:55:03 +0100

    Minor bugfix

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ while mainLoop:
         frame = np.rot90(frame)
         frame = cv2.flip(frame, 1)
 
-    match mainMenu["display"].data[mainMenu["display"].getCurrentVal()]:
+    match mainMenu["display"].possibleValues[mainMenu["display"].getCurrentVal()]:
         case "Edges":
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             
