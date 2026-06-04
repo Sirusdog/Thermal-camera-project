b4ac61e6d494d5f5742a905776d5e3090c5146b8
Sirusdog <sirusdog611@gmail.com>
2026-06-04 01:59:14 +0100

    Minor bugfix

--- helpers.py
+++ helpers.py
@@ -49,7 +49,7 @@ class MenuItem:
             case _:
                 raise ValueError("Invalid menu type specified.")
 
-        initialiseIncrementDecrementFunctions()
+        self.initialiseIncrementDecrementFunctions()
     
     def updateDisplayText(self, val):
         """Updates the displayed text to the given value. Mostly internal."""
