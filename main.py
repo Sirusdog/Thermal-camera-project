674a4f04d3e822bfdd24d76f3bdfde0e9e4afc99
Sirusdog <sirusdog611@gmail.com>
2026-06-04 13:52:52 +0100

    Bugfixing cutoff

--- main.py
+++ main.py
@@ -156,11 +156,12 @@ while mainLoop:
 
         case "Cutoff":
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            threshold = np.max(frame) * mainMenu["cutoff"].getCurrentVal()/100
+            threshold = np.max(frame) * (mainMenu["cutoff"].getCurrentVal()/100)
+            print(np.max(frame))
             for i in range(len(frame)):
                 frame[i][frame[i] < threshold] = 0
             img = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-            
+
         case "Full image":
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
