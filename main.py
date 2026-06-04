eede765333860a8cd5618f67621608977a9f97a4
Sirusdog <sirusdog611@gmail.com>
2026-06-04 15:45:08 +0100

    Temporarily removed pallet handeling

--- main.py
+++ main.py
@@ -177,8 +177,7 @@ while mainLoop:
             imgGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     curPallet = pallets[mainMenu["pallet"].getCurrentVal()]
-    img = np.array([[curPallet(v) for v in row] for row in imgGray])
-    print(img)
+    img = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
     img = cv2.resize(img, (coveredY, coveredX), interpolation = interpolationMode)
     #img = cv2.copyMakeBorder(img, yBuffer, yBuffer, xBuffer, 
     #xBuffer, cv2.BORDER_CONSTANT, value = (0,0,0))
