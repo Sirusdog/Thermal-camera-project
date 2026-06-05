a54dbeb3d94dd51695c79c3eaf9bbcd4af25c11e
Sirusdog <sirusdog611@gmail.com>
2026-06-05 22:43:14 +0100

    Minor bugfixes

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ class CameraHandler:
                     upperThreshold = mainMenu["edgeSensitivityUpper"].getCurrentVal()
 
                 edges = cv2.Canny(frame, lowerThreshold, upperThreshold)
-                img = recolorImage(edges, *curPallet)
+                img = recolorImage(edges, curPallet)
 
             elif displayMode == "Cutoff":
                 # Converts the image into grayscale, computes the threshold for the
@@ -222,11 +222,11 @@ class CameraHandler:
                 for i in range(len(frame)):
                     frame[i][frame[i] < threshold] = 0
 
-                img = recolorImage(frame, *curPallet)
+                img = recolorImage(frame, curPallet)
 
             elif displayMode == "Full image":
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                img = recolorImage(frame, *curPallet)
+                img = recolorImage(frame, curPallet)
 
             elif displayMode == "Raw output":
                 img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
