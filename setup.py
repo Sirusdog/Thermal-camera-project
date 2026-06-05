41effde01543ede4f4af4a5a5478c9480fb19507
Sirusdog <sirusdog611@gmail.com>
2026-06-05 21:36:53 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize("cythonFuncs.pyx"),
+    ext_modules = cythonize("cythonFuncs.pyx", compiler_directives = {
+        "boundscheck": False,
+        "wraparound": False
+    }, extra_compile_args=["-O3"]),
     include_dirs=[numpy.get_include()]
 )
