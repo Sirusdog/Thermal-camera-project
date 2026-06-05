5070164594d2521e3638f0440cc0b9c476cfe40d
Sirusdog <sirusdog611@gmail.com>
2026-06-05 20:40:05 +0100

    Properly imported numpy to cython file.

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("cythonFuncs.pyx")
+    ext_modules = cythonize("cythonFuncs.pyx"),
+    include_dirs=[numpy.get_include()]
 )
