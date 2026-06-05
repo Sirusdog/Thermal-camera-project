c115ef3b3a3254e9307286bcb1d4ec06afe44a6e
Sirusdog <sirusdog611@gmail.com>
2026-06-05 22:31:24 +0100

    Minor bugfix

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ class CameraHandler:
             displayMode = mainMenu["display"].getCurrentVal()
             edgeDetectMode = mainMenu["edgeDetectionMode"].getCurrentVal()
             curPallet = pallets[mainMenu["pallet"].getCurrentVal()]
-            rO, gO, bO, rM, gM, bM = [int(i) for i in curPallet]
+
 
             if displayMode =="Edges":
                 # Converts an image to grayscale and computes the threshold values
@@ -218,7 +218,7 @@ class CameraHandler:
                     upperThreshold = mainMenu["edgeSensitivityUpper"].getCurrentVal()
 
                 edges = cv2.Canny(frame, lowerThreshold, upperThreshold)
-                img = recolorImage(edges, rO, gO, bO, rM, gM, bM)
+                img = recolorImage(edges, *curpallet)
 
             elif displayMode == "Cutoff":
                 # Converts the image into grayscale, computes the threshold for the
@@ -228,11 +228,11 @@ class CameraHandler:
                 for i in range(len(frame)):
                     frame[i][frame[i] < threshold] = 0
 
-                img = recolorImage(frame, rO, gO, bO, rM, gM, bM)
+                img = recolorImage(frame, *curpallet)
 
             elif displayMode == "Full image":
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                img = recolorImage(frame, rO, gO, bO, rM, gM, bM)
+                img = recolorImage(frame, *curpallet)
 
             elif displayMode == "Raw output":
                 img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
