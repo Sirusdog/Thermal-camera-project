e5c2ddb6307359a814edcfe39a5f869418939003
Sirusdog <sirusdog611@gmail.com>
2026-06-27 17:14:04 +0100

    Minor bugfix

--- helpers.py
+++ helpers.py
@@ -247,7 +247,7 @@ class UARTController:
         }
     }
 
-    responsesGeneral = {v: k for k, v in commands.items()}
+    responsesGeneral = {v: k for k, v in list(commands.items())}
 
     crcCalculator = Calculator(Crc16.XMODEM)
 
