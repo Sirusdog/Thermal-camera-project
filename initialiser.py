e8436d3032029eaf8b4389ea7372c19f35f458d8
Sirusdog <sirusdog611@gmail.com>
2026-07-13 21:49:07 +0100

    Corrected initialiser code

--- initialiser.py
+++ initialiser.py
@@ -5,21 +5,19 @@
 from trapdoor import Trapdoor
 
 t = Trapdoor("main", "./configs", "mainConfig.toml")
-t.set("camSettings.enabledPallets", """White Hot, Black Hot, Ironbow, 
-    Rainbow, Night, Aurora, Red Hot, Jungle, Medical, Golden Red""") 
+t.set("camSettings.enabledPallets", """White Hot, Black Hot, Ironbow, Rainbow, Night, Aurora, Red Hot, Jungle, Medical, Golden Red""") 
 # Series of values seperated by a comma and a space 
 # UARTControllers command variable for pallets then it *will* crash.
 
-t.set("camSettings.enabledModes", """General, Outline, 
-    Low Temperature Highlight, Linear Stretch, High Contrast, Low Contrast""") 
+t.set("camSettings.enabledModes", """General, Outline, Low Temperature Highlight, Linear Stretch, High Contrast, Low Contrast""") 
 # Series of values seperated by a comma and a space
 # UARTControllers command variable for modes then it *will* crash.
 
 t.set("camSettings.camFovX", "17.6") # Float, get from camera datasheet
 t.set("camSettings.camFovY", "13.2") # Float, get from camera datasheet
 
-t.set("displaySettings.coveredX", "1000") # Int, from 0 to the screen size.
-t.set("displaySettings.coveredY", "1500") # Int, from 0 to the screen size.
+t.set("displaySettings.coveredX", "256") # Int, from 0 to the screen size.
+t.set("displaySettings.coveredY", "198") # Int, from 0 to the screen size.
 # Reccommend you compute coveredY based off of your camera's aspect ratio.
 # You are able to force this to be square, though it's not recomended.
 
