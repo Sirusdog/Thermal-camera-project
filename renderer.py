eb7f0a2a5770fa3241947b8322a28866a83b79fb
Sirusdog <sirusdog611@gmail.com>
2026-07-20 22:52:54 +0100

    Drew string2 before using quickchange

--- renderer.py
+++ renderer.py
@@ -112,7 +112,7 @@ font = pi3d.Font("../pi3d_demos/fonts/FreeSans.ttf", color="#FF8010")
 fps = "0"
 string2 = pi3d.String(camera=cam2D, is_3d=False, font=font, string=fps, 
 	x=-DISPLAY.width / 2 + 200, y=DISPLAY.height / 2 - 75, z=1.0)
-
+string2.draw()
 cube = pi3d.Cuboid(camera=cam, w = 10, h = 5, d = 20, x = 30)
 mykeys = pi3d.Keyboard()
 rot = [0,0,0]
