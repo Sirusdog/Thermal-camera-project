0b235dc50a99228648ee70cc65789cb3c267ccbd
Sirusdog <sirusdog611@gmail.com>
2026-07-20 22:54:25 +0100

    Brute forced enabling the BNO features

--- renderer.py
+++ renderer.py
@@ -88,13 +88,21 @@ from adafruit_bno08x import (
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
+
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 bno = BNO08X_I2C(i2c)
 
-bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-bno.enable_feature(BNO_REPORT_GYROSCOPE)
-bno.enable_feature(BNO_REPORT_MAGNETOMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+done = False
+while not done:
+	try:
+		bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+		bno.enable_feature(BNO_REPORT_GYROSCOPE)
+		bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+		bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+	except Exception as e:
+		print(e)
+	else:
+		done = True
 
 def quatToEuler(quaternion):
 	mag = np.linalg.norm(quaternion)
