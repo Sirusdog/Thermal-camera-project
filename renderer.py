fbed2cc26926b5ac84750f1ff8e63df85ff5635f
Sirusdog <sirusdog611@gmail.com>
2026-07-20 22:41:05 +0100

    Used 3D camera for cube rendering.

--- renderer.py
+++ renderer.py
@@ -113,7 +113,7 @@ fps = "0"
 string2 = pi3d.String(camera=cam2D, is_3d=False, font=font, string=fps, 
 	x=-DISPLAY.width / 2 + 200, y=DISPLAY.height / 2 - 75, z=1.0)
 
-cube = pi3d.Cuboid(w = 10, h = 5, l = 20, x = 30)
+cube = pi3d.Cuboid(w = 10, h = 5, l = 20, x = 30, camera=cam)
 mykeys = pi3d.Keyboard()
 rot = [0,0,0]
 last_tm = 0
