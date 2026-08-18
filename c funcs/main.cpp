3f2531c24381a86276a64352da75935925f2f71b
Sirusdog <sirusdog611@gmail.com>
2026-08-18 22:02:53 +0100

    Explicitly attributed raylib in main

--- c funcs/main.cpp
+++ c funcs/main.cpp
@@ -6,21 +6,21 @@
 
 using namespace std;
 using namespace cv;
-using namespace raylib;
+
 
 int main() {
     cout << "Running!" << endl;
     int screenX = GetScreenWidth();
     int screenY = GetScreenHeight();
 
-    InitWindow(screenX, screenY, "Thermal Cam Render");
+    raylib::InitWindow(screenX, screenY, "Thermal Cam Render");
 
     CameraController cam = CameraController(400, 400);
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        Texture2D imgTex = LoadTextureFromImage(cam.getFrame());
-        DrawTexture(imgTex, 0, 0, WHITE);
+    while (!raylib::WindowShouldClose()) {
+        raylib::BeginDrawing();
+        raylib::ClearBackground(raylib::RAYWHITE);
+        raylib::Texture2D imgTex = raylib::LoadTextureFromImage(cam.getFrame());
+        raylib::DrawTexture(imgTex, 0, 0, raylib::WHITE);
     };
     return 0;
 }
