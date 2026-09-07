19fc518dedc21d77ee49c0f21f97fd47b4097408
Sirusdog <sirusdog611@gmail.com>
2026-09-07 02:28:21 +0100

    Bug testing camera controller

--- helpers.py
+++ helpers.py
@@ -149,6 +149,8 @@ class CameraHandler:
         #)
 
         _, self.frame = self.cam.read()
+        if self.frame != None:
+            print("Frame is here?")
         #f = np.rot90(f)
         #frame = cv2.flip(f, 1)
         # cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
